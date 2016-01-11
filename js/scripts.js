095f91c94d05d4400dd2131077333590d67ee3cf
Blake Scott and Michael Schennum <Blakerscott@gmail.com>
2016-01-11 14:41:09 -0800

    added image appear and disappear

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,8 @@
 $(function() {
-  $("h1").click(function() {
-    alert("This is a header.");
-  });
 
-  $("h2").click(function() {
-    alert("this is header 2")
-  });
 
-  $("p").dblclick(function() {
-    alert("This is a paragraph.");
+  $(".clickable").click(function() {
+    $("#initially-showing").toggle();
+    $("#initially-hidden").toggle();
   });
-
-  $("img").hover(function() {
-    alert("This is an image.");
-  });
-
-  $("ul").hover(function() {
-    alert("This is an unordered list.");
-  });
-
 });
