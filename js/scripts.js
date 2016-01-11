01027ea19d3b6c810b3df58421194804472ec4ac
Blake Scott and Michael Schennum <Blakerscott@gmail.com>
2016-01-11 14:05:06 -0800

    add additional jquery functionality

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,20 @@ $(function() {
     alert("This is a header.");
   });
 
-  $("p").click(function() {
+  $("h2").click(function() {
+    alert("this is header 2")
+  });
+
+  $("p").dblclick(function() {
     alert("This is a paragraph.");
   });
 
-  $("img").click(function() {
+  $("img").hover(function() {
     alert("This is an image.");
   });
+
+  $("ul").hover(function() {
+    alert("This is an unordered list.");
+  });
+
 });
